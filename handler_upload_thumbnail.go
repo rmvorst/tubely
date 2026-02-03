92cfc580296dc612f65cda6fc6474996d8e67f3e
rmvorst <rmvorst@gmail.com>
2026-02-03 07:33:41 -0500

    Updated thumbnail storage to save the image server-side

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -41,12 +45,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	defer file.Close()
 
 	mediaType := header.Header.Get("Content-Type")
-	fileData, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to read file data", err)
-		return
-	}
-
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusNotFound, "Video not found", err)
@@ -57,13 +55,26 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	thumb := thumbnail{
-		data:      fileData,
-		mediaType: mediaType,
+	mediaType, _, err = mime.ParseMediaType(mediaType)
+	ext := strings.Split(mediaType, "/")
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "Media type not compatible", nil)
+		return
 	}
-	videoThumbnails[videoID] = thumb
 
-	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoIDString)
+	savePath := filepath.Join(cfg.assetsRoot, videoIDString+"."+ext[1])
+	newFile, err := os.Create(savePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating the file on the server", err)
+		return
+	}
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error copying file contents to server", err)
+		return
+	}
+
+	url := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoIDString, ext[1])
 	video.ThumbnailURL = &url
 
 	err = cfg.db.UpdateVideo(video)
